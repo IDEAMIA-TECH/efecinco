762533c2aa8f3efd607b60a639835ece8576ef5b
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-04-14 18:50:47 -0600

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -18,7 +18,7 @@ $servicios = $resultado->fetch_all(MYSQLI_ASSOC);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
-    <?php include('header.php'); ?>
+    <?php include('includes/header.php'); ?>
 
     <main>
         <section class="hero">
@@ -92,7 +92,7 @@ $servicios = $resultado->fetch_all(MYSQLI_ASSOC);
         </section>
     </main>
 
-    <?php include('footer.php'); ?>
+    <?php include('includes/footer.php'); ?>
 
     <style>
         .hero {
