e4aae546f5841ce01b66782048d95d81d91a275b
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-04-15 14:35:40 -0600

    Create alter_cotizaciones.sql

---
+++ sql/alter_cotizaciones.sql
@@ -0,0 +1,8 @@
+-- Agregar columna comentarios_admin a la tabla cotizaciones_camaras
+ALTER TABLE cotizaciones_camaras ADD COLUMN comentarios_admin TEXT;
+
+-- Agregar columna comentarios_admin a la tabla cotizaciones_acceso
+ALTER TABLE cotizaciones_acceso ADD COLUMN comentarios_admin TEXT;
+
+-- Agregar columna comentarios_admin a la tabla cotizaciones_cableado
+ALTER TABLE cotizaciones_cableado ADD COLUMN comentarios_admin TEXT; 
